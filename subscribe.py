4ca304aee7bd4bb421883195f248b8d2a81d0454
root <root@pigardendown.fritz.box>
2019-06-21 20:43:53 +0100

    improve connections

--- subscribe.py
+++ subscribe.py
@@ -29,21 +29,30 @@ def on_message(client, userdata, message):
       if "OFF" == msgReceived.decode():
          gpio.output(pin,OFF)
 
+def on_disconnect():
+   gpio.output(pin,OFF)
+
 client = mqtt.Client(thingid)
 
 client.on_message=on_message
+client.on_disconnect=on_disconnect
 
 client.will_set(thingTopic + "sys/state", "OFFLINE", qos=1, retain=True)
 
-client.connect(brokeraddr)
+client.connect(brokeraddr,keepalive=600)
 
-client.subscribe(commandTopic)
+client.publish(thingTopic + "env/thingid", thingid, qos=1, retain=True)
+client.publish(thingTopic + "env/brokeraddr", brokeraddr, qos=1, retain=True)
+client.publish(thingTopic + "env/pin", pin, qos=1, retain=True)
 
-client.publish(commandTopic, "ONLINE")
 client.publish(thingTopic, str(datetime.datetime.now()), qos=1, retain=True)
 client.publish(thingTopic + "sys/type", "actor", qos=1, retain=True)
 client.publish(thingTopic + "sys/device", "relais-switch_on-off", qos=1, retain=True)
 client.publish(thingTopic + "sys/state", "ONLINE", qos=1, retain=True)
+
+client.publish(commandTopic, "ONLINE")
+
+client.subscribe(commandTopic, qos=0)
 
 client.loop_forever()
 
